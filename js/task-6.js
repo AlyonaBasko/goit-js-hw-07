227329b196c7db62f1fe64f11e312fdcf4bc7e23
AlyonaBasko <33734752+AlyonaBasko@users.noreply.github.com>
2024-03-13 14:52:00 +0100

    update js and css task 6

--- js/task-6.js
+++ js/task-6.js
@@ -11,30 +11,36 @@ function getRandomHexColor() {
 }
 
 function createBoxes(amount) {
-  boxesContainer.innerHTML = ''; // Очищаємо попередні елементи перед створенням нових
-
+  boxesContainer.innerHTML = ''; 
   for (let i = 0; i < amount; i++) {
     const size = 30 + i * 10;
     const box = document.createElement('div');
     box.style.width = `${size}px`;
     box.style.height = `${size}px`;
-    box.style.backgroundColor = getRandomHexColor();
-    boxesContainer.appendChild(box);
+    box.style.backgroundColor = getRandomHexColor(); 
+    boxesContainer.appendChild(box); 
   }
 }
 
+
 function destroyBoxes() {
-  boxesContainer.innerHTML = ''; // Очищаємо вміст контейнера
+  boxesContainer.innerHTML = ''; 
 }
+createBtn.style.backgroundColor = '#4e75ff';
+destroyBtn.style.backgroundColor = '#ff4e4e';
+createBtn.style.color = '#fff';
+destroyBtn.style.color = '#fff';
+
 
 createBtn.addEventListener('click', () => {
   const amount = parseInt(input.value);
-  if (amount >= 1 && amount <= 100) {
+  if (amount >= 1 && amount <= 100) { 
     createBoxes(amount);
-    input.value = ''; // Очищаємо значення input
+    input.value = ''; 
   } else {
     alert('Please enter a number between 1 and 100.');
   }
 });
+
 
 destroyBtn.addEventListener('click', destroyBoxes);
